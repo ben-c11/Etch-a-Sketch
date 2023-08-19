71c9185792c3f1ebecae01e4a55a5ab1d1f4663d
ben <ben.clark11@protonmail.com>
2023-08-19 10:45:30 +0100

    working version of etch-a-sketch with flexbox container Learned about onlcick events and eventListeners: need to deepen this knowledge as parts were definitely trial and error. Encountered this keyword Encountered inputs and outputs and .value property Code structure Planning and designing a program from scratch Importing fonts Creating CSS shapes e.g. circles Dynamically creating HTML elements in the DOM via JS Many js concepts encountered and utilised in this project
    Future work: code clean up and restructure, create the etch pixel grid with grid css and figure out a more elaborate way to implement the rainbowToggle rather than having to repeat rainbowToggleFalse() function.
    Also add in a click-to-draw and click-to-stop function

--- files/script.js
+++ files/script.js
@@ -1,13 +1,25 @@
+bgColor = '#4c6a20';
+penColor = 'black';
 const gridContainer = document.querySelector('#grid-container');
-
 const gridSlider = document.querySelector('#slider');
-const gridSliderOutput = document.querySelector('output');
+const gridSliderOutput = document.querySelector('#output');
+const outputSlide = document.querySelector('#output-slide');
+let rainbowToggle = false;
+gridSliderOutput.innerText = gridSlider.value;  
+gridSlider.onchange = sliderUpdate;
 
-gridSliderOutput.innerText = gridSlider.value + 'x' + gridSlider.value;
+createGrid(16);
 
-function createGrid(){
+function sliderUpdate(){
+    gridSliderOutput.textContent = this.value;
+    gridSlider.value = this.value;
+
+}
+
+function createGrid(gridSize){
     //Generate grid square & give each one an ID                        
-    let gridSize = parseInt(gridSlider.value);
+    clearBoard();
+    gridSize = parseInt(gridSliderOutput.textContent);
     let gridCol;
     let gridRow;
     for(let i = 0; i < gridSize; i++){
@@ -15,30 +27,104 @@ function createGrid(){
         gridCol.classList.add('grid-col');
         gridCol.setAttribute('id','gridBox');
         gridContainer.append(gridCol);
+        gridCol.addEventListener('mouseover', colorGrid)
 
         for(let j = 0; j < gridSize; j++){
             gridRow = document.createElement('div')
             gridRow.classList.add('grid-row')
             gridRow.setAttribute('id','gridBox')
             gridCol.append(gridRow)
+            gridRow.addEventListener('mouseover', colorGrid)
         }
-    }
     
-    
-    /*
-    let gridSize = parseInt(gridSlider.value);
-    let gridBox;
-    for(let i = 0; i < gridSize**2; i++){
+        /*
+        let gridSize = parseInt(gridSlider.value);
+        let gridBox;
+        for(let i = 0; i < gridSize**2; i++){
         gridBox = document.createElement('div');
         gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
         gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
         gridContainer.insertAdjacentElement('afterbegin', gridBox);
-    }*/
-   
+        }*/
+    
 }
-createGrid()
 
 
+function changeSize(span){
+    createGrid(span)
+}
+
+//functionality of Set Size button
+SetSizeButton = document.querySelector('#set-size');
+gridSliderOutputInt = parseInt(gridSliderOutput.textContent);
+SetSizeButton.onclick = () => changeSize(gridSliderOutputInt);
+
+resetBtn = document.querySelector('.reset-btn');
+resetBtn.onclick = () => resetBoard();
+
+eraseBtn = document.querySelector('.erase-btn');
+eraseBtn.onclick = () => erase();
+
+randomBtn = document.querySelector('.random-btn');
+randomBtn.onclick = () => random();
+
+rainbowBtn = document.querySelector('.rainbow-btn');
+rainbowBtn.onclick = () => rainbow();
+
+
+function clearBoard(){
+    let board = document.querySelector('#grid-container');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
+    
+}
+
+
+
+function resetBoard(){
+    let board = document.querySelector('#grid-container');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.style.backgroundColor = bgColor);
+}
+
+
+
+function colorGrid(event){
+    if(rainbowToggle){
+        event.target.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    }else{
+        event.target.style.backgroundColor = penColor;
+    }
+    
+}
+
+//color palette response
+let colorPicker = document.querySelector('#color-picker')
+colorPicker.addEventListener("input", function(){
+    rainbowToggleFalse();
+    penColor = colorPicker.value;
+})
+
+
+function erase(){
+    rainbowToggleFalse();
+    penColor = bgColor;
+}
+
+function random(){
+    rainbowToggleFalse();
+    penColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+}
+
+function rainbow(){
+    rainbowToggle = true;
+    rainbowToggle != rainbowToggle;
+}
+
+function rainbowToggleFalse(){
+    rainbowToggle = false;
+}
+/*
 function colorGrid(color, gridBox){
     if(gridBox.buttons == 1){
         if(gridBox.target.classList == ''){
@@ -46,6 +132,5 @@ function colorGrid(color, gridBox){
             pixel.style.backgroundColor = color;
         }
     }
+}*/
 }
-
-
